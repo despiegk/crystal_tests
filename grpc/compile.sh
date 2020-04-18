e4da49aebaeafe400a674dfdcb0511c6e154d0cf
kristof <kristof@incubaid.com>
2020-04-18 04:50:39 +0200

    simulator

---
+++ grpc/compile.sh
@@ -0,0 +1,6 @@
+protoc -I protos \
+    --grpc_out=src/protobufs \
+    --crystal_out=src/protobufs \
+    --plugin=protoc-gen-grpc=bin/grpc_crystal \
+    --plugin=protoc-gen-crystal=bin/protoc-gen-crystal \
+    protos/hello_world.proto
